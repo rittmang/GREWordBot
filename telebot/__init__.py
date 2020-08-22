a64f0488bfc61136ce525b0c3bd4de89bc0ccb5a
rittmang <ritomgupta99@gmail.com>
2020-08-22 11:55:03 +0530

    Fresh start

---
+++ telebot/__init__.py
@@ -0,0 +1,55 @@
+from decouple import config
+from telegram import Update
+from telegram.ext import Updater
+
+
+# class for configuration of a bot
+class Config:
+    def __init__(self, token, port=False, load=None, no_load=None):
+        """
+        Function to initialize the config for a bot
+        :param token: Your bot token, as a string.
+        :param db_uri: Your database URL
+        :param webhook_url: The URL your webhook should connect to (only needed for webhook mode)
+        :param port: Port to use for your webhooks
+        """
+
+        self.TOKEN = token
+        #self.DB_URI = db_uri
+        #self.WEBHOOK_URL = webhook_url
+        self.PORT = port
+
+        self.LOAD = load
+        self.NO_LOAD = no_load
+
+
+def log(update: Update, func_name: str, extra_text: str = ""):
+    """
+    Function to log bot activity
+    :param update: Update object to retrieve info from
+    :param func_name: name of the function being called
+    :param extra_text: any extra text to be logged
+    :return: None
+    """
+    chat = "private chat"
+    if update.effective_chat.type != "private":
+        chat = update.effective_chat.title
+
+    print(update.effective_user.username, "called function", func_name, "from", chat)
+    if extra_text:
+        print(extra_text)
+
+
+# create config object
+config = Config(
+    token=config('TOKEN'),
+    #db_uri=config('DATABASE_URL'),
+    #webhook_url=config('WEBHOOK_URL', default=False),
+    port=config('PORT', default=False),
+    load=config('LOAD', default=False, cast=lambda x: x.split(" ") if x else False),
+    no_load=config('NO_LOAD', default=False, cast=lambda x: x.split(" ") if x else False),
+)
+
+# create updater and dispatcher
+updater = Updater(config.TOKEN, use_context=True)
+dispatcher = updater.dispatcher
---
+++ telebot/__main__.py
@@ -0,0 +1,165 @@
+import importlib
+from random import choice
+import urllib.request as request
+import json
+from telegram import Update, BotCommand
+from telegram.ext import CallbackContext, CommandHandler
+import os
+from telebot import updater, config, log, dispatcher
+
+# curl --data "entry.360220839=aa&entry.2084162701=bb&entry.2023085564=cc&entry.1938153099=dd" {os.environ['FORM_URL]}
+# mimic above in py to send formdata aa,bb,cc,dd to form db of form
+
+# default reply strings
+
+START_TEXT = f"""
+Hi there!
+I'm `{updater.bot.first_name}`, a bot to handle your GRE words.
+"""
+
+HELP_TEXT = (
+    START_TEXT
+    + """
+Use following commands to use me (*blush*):
+- /random - Get a random word from your list
+- /start - Turn me on
+"""
+)
+word_cache=[]
+
+def start(update: Update, context: CallbackContext):
+    # start message
+    log(update, func_name="start")
+    update.message.reply_markdown(START_TEXT)
+
+
+def help(update: Update, context: CallbackContext):
+    # display help message
+    log(update, func_name="help")
+
+    text_blob = "Nou"
+    update.message.reply_markdown(text_blob)
+
+
+def random(update: Update, context:CallbackContext):
+    #get a word from your Sheets
+    log(update,func_name="random")
+    
+    with request.urlopen(os.environ['SPREADSHEET_URL']) as response:
+        if response.getcode()==200:
+            source=response.read()
+            data=json.loads(source)
+            if(len(word_cache)==len(data['values'])):
+                word_cache.clear()
+            
+            while(True):
+                chosen=choice(range(len(data['values'])))
+                if data['values'][chosen][0] not in word_cache:
+                    MESSAGE=f"""
+*{data['values'][chosen][0]}*                  _{data['values'][chosen][2]}_
+------------------------------------------------
+`{data['values'][chosen][1]}`   
+
+{data['values'][chosen][3]}
+
+Hint=_{data['values'][chosen][4] if len(data['values'][chosen])==5 else ""}_
+            
+            """
+                    if(len(word_cache)==0):
+                        update.message.reply_markdown(f"[None in cache]")
+                    else:
+                        update.message.reply_markdown(f"_{word_cache}_")
+                    
+                    update.message.reply_markdown(MESSAGE)
+                    word_cache.append(data['values'][chosen][0])
+                    break
+                else:
+                    continue;
+        else:
+            MESSAGE=f"""Sheets API high or something..idk"""
+            update.message.reply_markdown(MESSAGE)
+
+def allWords(update:Update,context:CallbackContext):
+    #get all words from Sheets
+    log(update,func_name="all")
+    with request.urlopen(os.environ['SPREADSHEET_URL']) as response:
+        if response.getcode()==200:
+            source=response.read()
+            data=json.loads(source)
+            for i in range(1,len(data['values'])):
+                
+                MESSAGE=f"""
+{i}) *{data['values'][i][0]}*                  _{data['values'][i][2]}_
+------------------------------------------------
+`{data['values'][i][1]}`   
+
+{data['values'][i][3]}
+
+Hint=_{data['values'][i][4] if len(data['values'][i])==5 else ""}_
+            
+            """
+                update.message.reply_markdown(MESSAGE)
+        else:
+            MESSAGE=f"""Sheets API high or something..idk"""
+            update.message.reply_markdown(MESSAGE)
+
+def search(update:Update,context:CallbackContext):
+    # get substring matches from word / meaning field
+    log(update,func_name="search")
+    with request.urlopen(os.environ['SPREADSHEET_URL']) as response:
+        if response.getcode()==200:           
+            if(context.args):
+                count=0
+                source=response.read()
+                data=json.loads(source)
+                for i in range(1,len(data['values'])):
+                    if(data['values'][i][0].find(context.args[0])!=-1 or data['values'][i][1].find(context.args[0])!=-1):
+                        MESSAGE=f"""
+{i}) *{data['values'][i][0]}*                  _{data['values'][i][2]}_
+------------------------------------------------
+`{data['values'][i][1]}`   
+
+{data['values'][i][3]}
+
+Hint=_{data['values'][i][4] if len(data['values'][i])==5 else ""}_
+            
+                        """
+                        count=count+1
+                        update.message.reply_markdown(MESSAGE)
+                
+                if(count == 0):
+                    MESSAGE=f"""_Could not find requested word_"""
+                    update.message.reply_markdown(MESSAGE)
+                else:
+                    MESSAGE=f"""_{count} matches found_"""
+                    update.message.reply_markdown(MESSAGE)
+            else:
+                MESSAGE=f"""Uhhh.... I could search, but what?"""
+                update.message.reply_markdown(MESSAGE)
+        else:
+            MESSAGE=f"""Sheets API high or something..idk"""
+            update.message.reply_markdown(MESSAGE)
+
+
+# set bot commands
+COMMANDS = [
+    BotCommand(command='help', description="Display the help text to understand how to use this bot"),
+    BotCommand(command='random', description="Get a random word"),
+    BotCommand(command='all', description="Get all words in order"),
+    BotCommand(command='search',description="Gets words with this substring in word or meaning fields.")
+]
+
+if __name__ == "__main__":
+    # create handlers
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("random", random))
+    dispatcher.add_handler(CommandHandler("all",allWords))
+    dispatcher.add_handler(CommandHandler("search",search))
+    dispatcher.add_handler(CommandHandler("help", help))
+
+    updater.bot.set_my_commands(COMMANDS)
+
+    updater.start_polling()
+
+    print("WordBot thot")
+    updater.idle()
