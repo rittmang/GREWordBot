f8cca66fa8b34a0da0af037c9f3c7691a4772f18
ksdfg <ksdfg123@gmail.com>
2020-10-01 17:10:06 +0530

    fix mongoengine class and helper functions
    Signed-off-by: ksdfg <ksdfg123@gmail.com>

--- telebot/db/__init__.py
+++ telebot/db/__init__.py
@@ -1,3 +1,4 @@
+from traceback import print_exc
 from typing import List
 
 from mongoengine import DynamicDocument, IntField, ListField, StringField
@@ -7,7 +8,7 @@ from mongoengine import DynamicDocument, IntField, ListField, StringField
 
 
 class User(DynamicDocument):
-    id = IntField(required=True)
+    user_id = IntField(required=True, primary_key=True)
     words = ListField(StringField())
 
 
@@ -22,19 +23,20 @@ def add_word_for_user(user: int, word: str) -> bool:
     :return: True if successfully added, else False
     """
     try:
-        user_list = User.objects(id=user)
+        user_obj = User.objects(user_id=user).first()
 
-        if user_list:
+        if user_obj:
             # The user object already exists
-            user_list[0].words = user_list[0].words + [word]
-            user_list[0].save()
+            user_obj.words = user_obj.words + [word]
+            user_obj.save()
         else:
             # make new object
-            User(id=user, words=[word]).save()
+            User(user_id=user, words=[word]).save()
 
         return True
 
     except:
+        print_exc()
         return False
 
 
@@ -45,8 +47,9 @@ def get_words_for_user(user: int) -> List[str]:
     :return: List of all the words
     """
     try:
-        return User.objects(id=user)[0].words
+        return User.objects(user_id=user).first().words
     except:
+        print_exc()
         return []
 
 
@@ -57,11 +60,13 @@ def clear_words_for_user(user: int) -> bool:
     :return: True if all words cleared, else False
     """
     try:
-        user_list = User.objects(id=user)
+        user_list = User.objects(user_id=user).first()
         if user_list:
-            user_list[0].words = []
-            user_list[0].save()
+            user_list.words = []
+            user_list.save()
 
         return True
+
     except:
+        print_exc()
         return False
